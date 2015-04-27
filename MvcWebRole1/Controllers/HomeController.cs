f6d4b62e62f272acbc60658ded1e62dbe52e2a36
Sanjush <sanjush88@hotmail.com>
2015-04-27 16:29:50 +0200

    Fler kategorier och ingredienser

--- MvcWebRole1/Controllers/HomeController.cs
+++ MvcWebRole1/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage.Table.DataServices;
 using MvcWebRole1.Models;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.ServiceRuntime;
 
 
 
@@ -17,11 +18,22 @@ namespace MvcWebRole1.Controllers
 {
     public class HomeController : Controller
     {
-        BlobStorageServices _blobStorageServices = new BlobStorageServices();
+
+        public CloudBlobContainer GetCloudBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("ReceptConnection"));
+            CloudBlobClient blobclient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer blobContainer = blobclient.GetContainerReference("myimages");
+            if (blobContainer.CreateIfNotExists())
+            {
+                blobContainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            }
+            return blobContainer;
+        }
 
         public ActionResult upLoad(string kategori, string receptnamn, string inloggningsnamn, string ingrediens1, string ingrediensmangd1, string mattenhet1, string antal, string image)
         {
-            CloudBlobContainer blobContainer = _blobStorageServices.GetCloudBlobContainer();
+            CloudBlobContainer blobContainer = GetCloudBlobContainer();
             List<string> blobs = new List<string>();
             foreach (var blobItem in blobContainer.ListBlobs())
             {
@@ -30,72 +42,94 @@ namespace MvcWebRole1.Controllers
 
             ViewBag.Message = kategori;
 
-            var kat = new List<string>();
-            kat.Add("Pannkaka");
-            kat.Add("Soppa");
-            kat.Add("Glass");
-            ViewBag.kategori = new SelectList(kat);
+            category();
 
             var in1 = new List<string>();
             in1.Add("Mjölk");
             in1.Add("Salt");
+            in1.Add("Svartpeppar");
+            in1.Add("Curry");
             in1.Add("Ägg");
+            in1.Add("Smör");
+            in1.Add("Vetemjöl");
+            in1.Add("Rågmjöl");
+            in1.Add("Vetemjöl Special");
+            in1.Add("Brödsirap");
+            in1.Add("Vispgrädde");
+            in1.Add("Choklad");
+            in1.Add("Hallon");
+            in1.Add("Matyogurt");
+            in1.Add("Strösocker");
+            in1.Add("Wokgrönskaer");
+            in1.Add("Ris");
+            in1.Add("Potatis");
+            in1.Add("Kycklingfilé");
+            in1.Add("Hönsbuljong");
+            in1.Add("Matlagningsgrädde");
+            in1.Add("Creme fraiche");
+            in1.Add("Jäst");
+            in1.Add("Honung");
+            in1.Add("Vatten");
+            in1.Add("Gräslök");
+            in1.Add("Persilja");
+            in1.Add("Krossade tomater");
+            in1.Add("Olivolja");
+            in1.Add("Valmofrön");
+            in1.Add("Pasta");
+            in1.Add("Ost");
+            in1.Add("Ströbröd");
+            in1.Add("Lammkotletter");
+            in1.Add("Lammfärs");
+            in1.Add("Rödlök");
+            in1.Add("Gul lök");
+            in1.Add("Vitlök");
+            in1.Add("Rosmarin");
+            in1.Add("Citron");
+            in1.Add("Rödvin");
+            in1.Add("Vitt vin");
+            in1.Add("Schalottenlök");
+            in1.Add("Oxfile");
+            in1.Add("Lax");
+            in1.Add("Spenat");
+            in1.Add("Championer");
+            in1.Add("Kantareller");
             ViewBag.ingrediens1 = new SelectList(in1);
 
             var matt1 = new List<string>();
             matt1.Add("dl");
             matt1.Add("cl");
             matt1.Add("krm");
+            matt1.Add("tsk");
             matt1.Add("st");
             ViewBag.mattenhet1 = new SelectList(matt1);
 
             if(!String.IsNullOrEmpty(kategori)){
-            /*CloudStorageAccount obj_Account = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("ConnectionString"));
-            CloudTableClient cloud_Table;
-            CloudTable table;
-            Recept obj_Entity = new Recept();
-            TableOperation insertOperation;
-
-            cloud_Table = obj_Account.CreateCloudTableClient();
-            table = cloud_Table.GetTableReference("Recept");
-            table.CreateIfNotExists();
-
-            obj_Entity.ReceptNamn = receptnamn;
-            obj_Entity.Inloggnamn = inloggningsnamn;
-            if (ingrediens1.Equals("Mjölk"))
-            {
-                obj_Entity.mjolk = ingrediensmangd1;
-                obj_Entity.mjolkmatt = mattenhet1;
-            }
-            else if (ingrediens1.Equals("Salt"))
-            {
-                obj_Entity.salt = ingrediensmangd1;
-                obj_Entity.saltmatt = mattenhet1;
-            }
-            else if (ingrediens1.Equals("Ägg"))
-            {
-                obj_Entity.agg = ingrediensmangd1;
-                obj_Entity.aggmatt = mattenhet1;
-            }
-
-            obj_Entity.antal = int.Parse(antal);
-            obj_Entity.blobnamn = image;
-            obj_Entity.PartitionKey = kategori;
-            obj_Entity.RowKey = receptnamn;
-            insertOperation = TableOperation.Insert(obj_Entity);
-            table.Execute(insertOperation);*/
+            
         }
            
             return View(blobs);
         }
 
-        
+        public void category()
+        {
+            var kat = new List<string>();
+            kat.Add("Pannkakor");
+            kat.Add("Soppor");
+            kat.Add("Surdegsbröd");
+            kat.Add("Kyckling");
+            kat.Add("Bröd");
+            kat.Add("Lamm");
+            kat.Add("Oxfile");
+            kat.Add("Lax");
+            kat.Add("Glass");
+            ViewBag.kategori = new SelectList(kat);
+        }
 
         [HttpPost]
         public ActionResult upLoad(HttpPostedFileBase image, string antal, string kategori, string receptnamn, string inloggningsnamn, string ingrediens1, string ingrediensmangd1, string mattenhet1)
         {
             string blobName = image.FileName + inloggningsnamn;
-            CloudBlobContainer blobContainer = _blobStorageServices.GetCloudBlobContainer();
+            CloudBlobContainer blobContainer = GetCloudBlobContainer();
             CloudBlockBlob blob = blobContainer.GetBlockBlobReference(blobName);
             blob.UploadFromStream(image.InputStream);
 
@@ -145,7 +179,7 @@ namespace MvcWebRole1.Controllers
             Uri uri = new Uri(name);
             string filename = System.IO.Path.GetFileName(uri.LocalPath);
 
-            CloudBlobContainer blobContainer = _blobStorageServices.GetCloudBlobContainer();
+            CloudBlobContainer blobContainer = GetCloudBlobContainer();
             CloudBlockBlob blob = blobContainer.GetBlockBlobReference(filename);
 
             blob.Delete();
@@ -161,7 +195,7 @@ namespace MvcWebRole1.Controllers
             CloudTable table = tableClient.GetTableReference("Recept");
             TableQuery<Recept> query = new TableQuery<Recept>();
 
-            CloudBlobContainer blobContainer = _blobStorageServices.GetCloudBlobContainer();
+            CloudBlobContainer blobContainer = GetCloudBlobContainer();
             List<string> blobs = new List<string>();
 
             ViewBag.Recipedata = table.ExecuteQuery(query);
--- MvcWebRole1/Models/Recept.cs
+++ MvcWebRole1/Models/Recept.cs
@@ -19,14 +19,108 @@ namespace MvcWebRole1.Models
 
         public string ReceptNamn { get; set; }
         public string Inloggnamn { get; set; }
+        public string Kommentar { get; set; }
+
         public string mjolk { get; set; }
         public string salt { get; set; }
         public string agg { get; set; }
+        public string svartpeppar { get; set; }
+        public string curry { get; set; }
+        public string smor { get; set; }
+        public string vetemjol { get; set; }
+        public string rogmjol { get; set; }
+        public string vetemjolspecial { get; set; }
+        public string brodsirap { get; set; }
+        public string vispgradde { get; set; }
+        public string choklad { get; set; }
+        public string hallon { get; set; }
+        public string matyogurt { get; set; }
+        public string strosocker { get; set; }
+        public string wokgronsaker { get; set; }
+        public string ris { get; set; }
+        public string potatis { get; set; }
+        public string kycklingfile { get; set; }
+        public string honsbuljong { get; set; }
+        public string matlagningsgradde { get; set; }
+        public string cremefraiche { get; set; }
+        public string jast { get; set; }
+        public string honung { get; set; }
+        public string vatten { get; set; }
+        public string graslok { get; set; }
+        public string persilja { get; set; }
+        public string krossadetomater { get; set; }
+        public string olivolja { get; set; }
+        public string valmofron { get; set; }
+        public string pasta { get; set; }
+        public string ost { get; set; }
+        public string strobrod { get; set; }
+        public string lammkotletter { get; set; }
+        public string lammfars { get; set; }
+        public string rodlok { get; set; }
+        public string gullok { get; set; }
+        public string vitlok { get; set; }
+        public string rosmarin { get; set; }
+        public string citron { get; set; }
+        public string rodvin { get; set; }
+        public string vittvin { get; set; }
+        public string schalottenlok { get; set; }
+        public string oxfile { get; set; }
+        public string lax { get; set; }
+        public string spenat { get; set; }
+        public string champinjoner { get; set; }
+        public string kantareller { get; set; }
+        
                 
 
         public string mjolkmatt { get; set; }
         public string saltmatt { get; set; }
         public string aggmatt { get; set; }
+        public string svartpepparmatt { get; set; }
+        public string currymatt { get; set; }
+        public string smormatt { get; set; }
+        public string vetemjölmatt { get; set; }
+        public string rogmjolmatt { get; set; }
+        public string vetemjolspecialmatt { get; set; }
+        public string brodsirapmatt { get; set; }
+        public string vispgraddematt { get; set; }
+        public string chokladmatt { get; set; }
+        public string hallonmatt { get; set; }
+        public string matyogurtmatt { get; set; }
+        public string strosockermatt { get; set; }
+        public string wokgronsakermatt { get; set; }
+        public string rismatt { get; set; }
+        public string potatismatt { get; set; }
+        public string kycklingfilematt { get; set; }
+        public string honsbuljongmatt { get; set; }
+        public string matlagningsgraddematt { get; set; }
+        public string cremefraichematt { get; set; }
+        public string jastmatt { get; set; }
+        public string honungmatt { get; set; }
+        public string vattenmatt { get; set; }
+        public string graslokmatt { get; set; }
+        public string persiljamatt { get; set; }
+        public string krossadetomatermatt { get; set; }
+        public string olivoljamatt { get; set; }
+        public string valmofronmatt { get; set; }
+        public string pastamatt { get; set; }
+        public string ostmatt { get; set; }
+        public string strobrodmatt { get; set; }
+        public string lammkotlettermatt { get; set; }
+        public string lammfarsmatt { get; set; }
+        public string rodlokmatt { get; set; }
+        public string gullokmatt { get; set; }
+        public string vitlokmatt { get; set; }
+        public string rosmarinmatt { get; set; }
+        public string citronmatt { get; set; }
+        public string rodvinmatt { get; set; }
+        public string vittvinmatt { get; set; }
+        public string schalottenlokmatt { get; set; }
+        public string oxfilematt { get; set; }
+        public string laxmatt { get; set; }
+        public string spenatmatt { get; set; }
+        public string champinjonermatt { get; set; }
+        public string kantarellermatt { get; set; }
+
 
         public int antal { get; set; }
         public string blobnamn { get; set; }
